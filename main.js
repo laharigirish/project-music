4ba4c169818459d811ff02cab3cf33f368abe097
laharigirish <96625142+laharigirish@users.noreply.github.com>
2023-06-11 12:10:38 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+song1="";
+song2="";
+leftWristX=0;
+leftWristY=0;
+rightWristX=0;
+rightWristY=0;
+
+function preload(){
+    song1= loadSound("music.mp3");
+    song2= loadSound("music2.mp3");
+}
+
+
+function setup(){
+    canvas= createCanvas(600, 500);
+    canvas.center();
+    video= createCapture(VIDEO);
+    video.hide();
+    posenet= ml5.poseNet(video, modelLoaded);
+    posenet.on('pose', gotPoses);
+}
+
+function modelLoaded(){
+    console.log("Pose net initialized");
+}
+
+function draw(){
+    image(video, 0, 0, 600, 500);
+}
+
+function play(){
+    song1.play();
+    song2.play();
+    
+
+}
+
+function gotPoses(error, results){
+if(results.length>0){
+    console.log(results);
+}
+}
